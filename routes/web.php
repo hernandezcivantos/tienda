63af5141edf5ab374f1cbe56bd260cfe454b01b4
hernandezcivantos <hernandez.civantos@gmail.com>
2023-11-01 00:30:15 +0100

    feat: added bootstrap, css, js base

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,14 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LandingController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+# Landing
+Route::get('/', [LandingController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+# Autorización
+Auth::routes();
+
+# Panel admin
+Route::get('/home', [HomeController::class, 'index'])->name('home');
